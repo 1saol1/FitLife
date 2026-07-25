30c38a8a4ab712bfa04c62483db30a7619c29eab
1saol1 <flane367fr@gmail.com>
2026-07-25 20:38:21 +0300

    изменение для решения проблемы с тестами

--- fit_life.py
+++ fit_life.py
@@ -1,3 +1,10 @@
+import sys
+
+# было выполнено через нейронку, т.к. не проходил тест
+sys.stdout.reconfigure(encoding="utf-8")
+sys.stderr.reconfigure(encoding="utf-8")
+
+
 WATER_PER_KG = 30
 ML_PER_L = 1000
 
@@ -5,7 +12,7 @@ print("Здравствуйте!")
 print("Эта программа рассчитывает ваш индекс массы тела.")
 print("Для корректной работы программы заполните несколько полей")
 
-user_name = input("Введите своё имя: ")
+user_name = input("Введите свое имя: ")
 user_age = int(input("Введите свой возраст: "))
 user_weight = float(input("Введите вес (кг, например: 60.4): "))
 user_height = float(input("Введите рост (м, например: 1.78): "))
@@ -19,7 +26,7 @@ water_ml = user_weight * WATER_PER_KG
 # Перевод миллилитры в литры
 water_l = water_ml / ML_PER_L
 
-print(f"Отчёт для пользователя: {user_name.title()} ({user_age} г.)")
+print(f"Отчет для пользователя: {user_name.title()} ({user_age} г.)")
 print(f"Ваш индекс массы тела: {bmi}")
 print(f"Рекомендуемая норма воды: {water_l} л. в день")
-print("\nРасчёт окончен. Будьте здоровы!")
+print("\nРасчет окончен. Будьте здоровы!")
